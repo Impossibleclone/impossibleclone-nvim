b8fb42de52db6ec95b8f7617d167332f82b0e678
impossibleclone <impossibleclone2005@gmail.com>
2025-07-17 14:10:26 +0530

    some customisations

--- config/options.lua
+++ config/options.lua
@@ -1,6 +1,10 @@
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 vim.opt.mouse = 'a'
+-- vim.opt.guicursor = ""
+vim.opt.cursorline = true
+-- vim.opt.colorcolumn = ""
+vim.opt.wrap = false
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -15,6 +19,7 @@ vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.termguicolors = true
 vim.opt.showmode = false
+vim.opt.autochdir = true
 
 vim.opt.incsearch = true
 vim.opt.hlsearch = false
--- plugins/lualine.lua
+++ plugins/lualine.lua
@@ -1,14 +1,31 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "catppuccin",
-        section_separators = "",
-        component_separators = "|",
-      },
-    })
-  end,
-}
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            options = {
+                theme = "catppuccin",
+                section_separators = { left = '', right = '' },
+                component_separators = { left = '\\', right = '/' },
 
+                { fmt = string.lower },
+            },
+            sections = {
+                lualine_a = { { 'mode', fmt = function(str) return str:sub(1, 1) end, separator = { left = ' ⏽ ', right = '' }, component_separators = { right = '⏽', left = '' }, right_padding = 2 } },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'lsp_status', 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { { 'location', separator = { right = ' ⏽ ', left = '' }, left_padding = 2 } },
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { 'filename' },
+                lualine_x = { 'location' },
+                lualine_y = {},
+                lualine_z = {}
+            },
+        })
+    end,
+}
