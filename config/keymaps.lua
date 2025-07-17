335ef89022659d07aea72e2307690ca46321ca44
impossibleclone <impossibleclone2005@gmail.com>
2025-07-17 19:28:13 +0530

    ok

--- config/keymaps.lua
+++ config/keymaps.lua
@@ -5,8 +5,14 @@ local opts = { noremap = true, silent = true }
 map("n", "<Esc>", "<cmd>nohlsearch<CR>", opts)
 map("n", "<leader>w", "<cmd>w<CR>", opts)
 map("n", "<leader>q", "<cmd>q<CR>", opts)
+map("n", "<leader>e", ":Ex<CR>", opts)
 map("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find files" })
 map("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Live grep" })
+
+map("n", "<A-h>", ":ToggleTerm direction=horizontal<CR>", opts)
+map("n", "<A-v>", ":ToggleTerm direction=vertical <CR>", opts)
+map("n", "<A-i>", ":ToggleTerm direction=float<CR>", opts)
+
 map("n", "<C-h>", "<C-w>h", opts)
 map("n", "<C-j>", "<C-w>j", opts)
 map("n", "<C-k>", "<C-w>k", opts)
@@ -27,3 +33,20 @@ map("n", "<leader>rn", vim.lsp.buf.rename, opts)
 map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 map("n", "gr", vim.lsp.buf.references, opts)
 map("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, opts)
+function _G.set_terminal_keymaps()
+    local opts = { buffer = 0 }
+    map('t', '<esc>', [[<C-\><C-n>]], opts)
+    map("t", "<C-c>", [[<C-\><C-n>]], opts)
+    map('t', 'jk', [[<C-\><C-n>]], opts)
+    map('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+    map('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+    map('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+    map('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+    map('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    map("t", "<A-h>", [[<Cmd>wincmd q<CR>]], opts)
+    map("t", "<A-v>", [[<Cmd>wincmd q<CR>]], opts)
+    map("t", "<A-i>", [[<Cmd>wincmd q<CR>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
