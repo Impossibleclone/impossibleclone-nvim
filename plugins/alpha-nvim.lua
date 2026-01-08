50206f5427ccf06af9d450761a9a195a67d5e5c7
impossibleclone <impossibleclone2005@gmail.com>
2026-01-08 16:49:22 +0530

    updated Alpha-nvim header

--- plugins/alpha-nvim.lua
+++ plugins/alpha-nvim.lua
@@ -6,34 +6,21 @@ return {
         local dashboard = require("alpha.themes.dashboard")
         local alpha = require("alpha")
 
-        -- Read header from curl | sed
-        -- local function get_clean_ascii_art()
-        --     local cmd = "ping -q -c 1 -W 1 1.1.1.1 >/dev/null 2>&1 && curl -sS pkmn.li/icons/ | sed '1,3d'"
-        --     local handle = io.popen(cmd)
-        --     if not handle then return { "Failed to fetch Pokémon" } end
-        --
-        --     local result = handle:read("*a")
-        --     handle:close()
-        --
-        --     local lines = {}
-        --     for line in result:gmatch("[^\r\n]+") do
-        --         -- Strip ANSI escape codes like \x1b[31m
-        --         line = line:gsub("\27%[[%d;]*m", "")
-        --         table.insert(lines, line)
-        --     end
-        --
-        --     return lines
-        -- end
-        --
-        -- dashboard.section.header.val = get_clean_ascii_art()
+dashboard.section.header.val = {
+    [[                                                                                                         ]],
+    [[   _                               _ _     _           _                       _   _       _             ]],
+    [[  (_)_ __ ___  _ __   ___  ___ ___(_) |__ | | ___  ___| | ___  _ __   ___     | \ | |_   _(_)_ __ ___    ]],
+    [[  | | '_ ` _ \| '_ \ / _ \/ __/ __| | '_ \| |/ _ \/  _| |/ _ \| '_ \ / _ \____|  \| \ \ / / | '_ ` _ \   ]],
+    [[  | | | | | | | |_) | (_) \__ \__ \ | |_) | |  __/| |_| | (_) | | | |  __/____| |\  |\ V /| | | | | | |  ]],
+    [[  |_|_| |_| |_| .__/ \___/|___/___/_|_.__/|_|\___|\___|_|\___/|_| |_|\___|    |_| \_| \_/ |_|_| |_| |_|  ]],
+    [[              |_|                                                                                        ]],
+}
 
-
-        -- Set menu
         dashboard.section.buttons.val = {
             dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-            dashboard.button("ldr ff", "  > Find file", ":cd $HOME/prgm/ | Telescope find_files<CR>"),
-            dashboard.button("ldr fo", "  > Recent", ":Telescope oldfiles<CR>"),
-            dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+            dashboard.button("ff", "  > Find file", ":cd $HOME/prgm/ | Telescope find_files<CR>"),
+            dashboard.button("fo", "  > Recent", ":Telescope oldfiles<CR>"),
+            dashboard.button("q", "󰗼  > Quit NVIM", ":qa<CR>"),
         }
 
         alpha.setup(dashboard.opts)
