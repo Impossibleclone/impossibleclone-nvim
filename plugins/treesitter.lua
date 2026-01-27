b2adc485f4a5c5ed9bf6e3a2ad7cb15478af053c
impossibleclone <impossibleclone2005@gmail.com>
2026-01-28 00:14:35 +0530

    telescope better with extension and removed pets and added transparent.nvim

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("nvim-treesitter.config").setup({
       ensure_installed = {
-        "lua", "python", "typescript", "javascript", "html", "css", "json", "go", "rust", "java", "php", "c", "cpp", "cmake", "zsh",
+        "lua", "python", "typescript", "javascript", "html", "css", "json", "go", "rust", "java", "php", "c", "cpp", "cmake", "zsh","r","markdown","rnoweb","yaml",
       },
       highlight = { enable = true },
       indent = { enable = true },
