cb06bf4a7ed61513b1c7610b8f64d2217c466cf8
impossibleclone <impossibleclone2005@gmail.com>
2026-03-01 23:52:49 +0530

    updates to keymaps added comments

--- config/keymaps.lua
+++ config/keymaps.lua
@@ -3,45 +3,45 @@ vim.g.mapleader = " "
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 map("n", "<Esc>", "<cmd>nohlsearch<CR>", opts)
-map("n", "<leader>w", "<cmd>w<CR>", opts)
-map("n", "<leader>q", "<cmd>q<CR>", opts)
-map("n", "<leader>Q", "<cmd>q!<CR>", opts)
+map("n", "<leader>w", "<cmd>w<CR>", opts)  -- Save
+map("n", "<leader>q", "<cmd>q<CR>", opts)  -- Quit
+map("n", "<leader>Q", "<cmd>q!<CR>", opts) -- force quit
 
-map("n", "<leader>d", "<cmd>TransparentToggle<CR>", opts)
-map("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find files" })
-map("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Live grep" })
-map("n", "<leader>fo", ":Telescope oldfiles<CR>", { desc = "Old files" })
+map("n", "<leader>d", "<cmd>TransparentToggle<CR>", opts) -- Transparent toggle
+map("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find files" }) -- Find files
+map("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Live grep" }) -- search for words
+map("n", "<leader>fo", ":Telescope oldfiles<CR>", { desc = "Old files" }) -- find old files
 
-map("n", "<A-h>", ":ToggleTerm direction=horizontal<CR>", opts)
-map("n", "<A-v>", ":ToggleTerm direction=vertical <CR>", opts)
-map("n", "<A-i>", ":ToggleTerm direction=float<CR>", opts)
+map("n", "<A-h>", ":ToggleTerm direction=horizontal<CR>", opts) -- Horizontal terminal
+map("n", "<A-v>", ":ToggleTerm direction=vertical <CR>", opts) -- Vertical terminal
+map("n", "<A-i>", ":ToggleTerm direction=float<CR>", opts) -- Float terminal
 
-map("n", "<C-h>", "<C-w>h", opts)
-map("n", "<C-j>", "<C-w>j", opts)
-map("n", "<C-k>", "<C-w>k", opts)
-map("n", "<C-l>", "<C-w>l", opts)
-map("n", "<C-Up>", ":resize -2<CR>", opts)
-map("n", "<C-Down>", ":resize +2<CR>", opts)
-map("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-map("n", "<C-Right>", ":vertical resize +2<CR>", opts)
-map("n", "<C-d>", "<C-d>zz")
-map("n", "<C-u>", "<C-u>zz")
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
+map("n", "<C-h>", "<C-w>h", opts) -- Move to left window
+map("n", "<C-j>", "<C-w>j", opts) -- Move to bottom window
+map("n", "<C-k>", "<C-w>k", opts) -- Move to top window
+map("n", "<C-l>", "<C-w>l", opts) -- Move to right window
+map("n", "<C-Up>", ":resize -2<CR>", opts) -- Resize window up
+map("n", "<C-Down>", ":resize +2<CR>", opts) -- Resize window down
+map("n", "<C-Left>", ":vertical resize -2<CR>", opts) -- Resize window left
+map("n", "<C-Right>", ":vertical resize +2<CR>", opts) -- Resize window right
+map("n", "<C-d>", "<C-d>zz") -- Scroll half a page down
+map("n", "<C-u>", "<C-u>zz") -- Scroll half a page up
+map("v", "J", ":m '>+1<CR>gv=gv") -- Move down
+map("v", "K", ":m '<-2<CR>gv=gv") -- Move up
 
 
-map("n", "<leader>tn", ":tabnew<CR>", opts)
-map("n", "<leader>tc", ":tabclose<CR>", opts)
-map("n", "<leader>to", ":tabonly<CR>", opts)
-map("n", "<leader>]", ":tabnext<CR>", opts)
-map("n", "<leader>[", ":tabprev<CR>", opts)
-map("n", "gd", vim.lsp.buf.definition, opts)
-map("n", "K", vim.lsp.buf.hover, opts)
-map("n", "gi", vim.lsp.buf.implementation, opts)
-map("n", "<leader>rn", vim.lsp.buf.rename, opts)
-map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-map("n", "gr", vim.lsp.buf.references, opts)
-map("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, opts)
+map("n", "<leader>tn", ":tabnew<CR>", opts) -- New tab
+map("n", "<leader>tc", ":tabclose<CR>", opts) -- Close tab
+map("n", "<leader>to", ":tabonly<CR>", opts) -- Close all tabs
+map("n", "<leader>]", ":tabnext<CR>", opts) -- Next tab
+map("n", "<leader>[", ":tabprev<CR>", opts) -- Previous tab
+map("n", "gd", vim.lsp.buf.definition, opts) -- Go to definition
+map("n", "K", vim.lsp.buf.hover, opts) -- Hover
+map("n", "gi", vim.lsp.buf.implementation, opts) -- Go to implementation
+map("n", "<leader>rn", vim.lsp.buf.rename, opts) -- Rename
+map("n", "<leader>ca", vim.lsp.buf.code_action, opts) -- Code action
+map("n", "gr", vim.lsp.buf.references, opts) -- References
+map("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, opts) -- Format
 function _G.set_terminal_keymaps()
     local opts = { buffer = 0 }
     map('t', '<esc>', [[<C-\><C-n>]], opts)
